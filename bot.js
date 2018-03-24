e6b34cee359cc5cf5de426ef0f073535a7955d71
palextiago19 <37705893+palextiago19@users.noreply.github.com>
2018-03-24 15:06:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '!Server') 
         
-        send(message.channel, 'Server em Construção!',{name: 'Manutenção', icon: 'https://cdn.discordapp.com/attachments/427100723130531840/427100781255065600/X9eAmHm.png'});
+        //send(message.channel, 'Server em Construção!',{name: 'Manutenção', icon: 'https://cdn.discordapp.com/attachments/427100723130531840/427100781255065600/X9eAmHm.png'});
     	
     //message.channel.send({embed: {
   //color: 3447003,
@@ -17,6 +17,36 @@ client.on('message', message => {
     
     //message.reply('Server em Construção!');
   	//}
+        message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+        
 });
 
 // THIS  MUST  BE  THIS  WAY
